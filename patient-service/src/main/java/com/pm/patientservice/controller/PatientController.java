1e6d2ebb38d3689ed10536294ce65ddd3c4d61ac
marko <markopopov1993@gmail.com>
2025-06-01 21:29:15 +0200

    Finish swagger descriptions.

--- patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -4,6 +4,8 @@ import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.dto.validators.CreatePatientValidationGroup;
 import com.pm.patientservice.service.PatientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.groups.Default;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -14,6 +16,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/patients")
+@Tag(name = "Patient", description = "API for managing Patients")
 public class PatientController {
 
   private final PatientService patientService;
@@ -23,12 +26,14 @@ public class PatientController {
   }
 
   @GetMapping
+  @Operation(summary = "Get all Patients")
   public ResponseEntity<List<PatientResponseDTO>> getPatients() {
     List<PatientResponseDTO> patients = patientService.getPatients();
     return ResponseEntity.ok(patients);
   }
 
   @PostMapping
+  @Operation(summary = "Create a new Patient")
   public ResponseEntity<PatientResponseDTO> createPatient(
       @Validated({Default.class, CreatePatientValidationGroup.class})
       @RequestBody PatientRequestDTO requestDTO) {
