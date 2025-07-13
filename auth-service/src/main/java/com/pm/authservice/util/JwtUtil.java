6bdf7296f453fb5db4c3b73b222d65c750038cee
Maximuss1993 <markopopov1993@gmail.com>
2025-07-13 15:25:57 +0200

    Token validation implemented..

--- auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
+++ auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
@@ -1,10 +1,13 @@
 package com.pm.authservice.util;
 
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Base64;
@@ -31,5 +34,18 @@ public class JwtUtil {
         .signWith(securityKey)
         .compact();
   }
+
+  public void validateToken(String token) {
+    try {
+      Jwts.parser().verifyWith((SecretKey) securityKey)
+          .build()
+          .parseSignedClaims(token);
+    } catch (SignatureException e) {
+      throw new JwtException("Invalid JWT Signature");
+    } catch (JwtException e) {
+      throw new JwtException("Invalid JWT");
+    }
+  }
+
 
 }
